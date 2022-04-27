5056e4315effe9a9c023acb3c1cb01381767bba9
keithalewis <kal@kalx.net>
2022-04-27 19:12:01 -0400

    connect, query

--- xll_mysql.cpp
+++ xll_mysql.cpp
@@ -1,4 +1,66 @@
 // xll_mysql.cpp
+#pragma comment (lib, "crypt32")
 #include "xll_mysql.h"
 
 using namespace xll;
+
+AddIn xai_mysql_connect(
+	Function(XLL_HANDLEX, "xll_mysql_connect", "\\MYSQL.CONNECT")
+	.Arguments({
+		Arg(XLL_CSTRING4, "host", "is the database host name."),
+		Arg(XLL_CSTRING4, "user", "is the user name."),
+		Arg(XLL_CSTRING4, "pass", "is the password."),
+		Arg(XLL_CSTRING4, "_db", "is the optional database name."),
+		})
+	.Uncalced()
+	.Category(CATEGORY)
+	.FunctionHelp("Return a handle to a MySQL database connection.")
+	.HelpTopic("https://dev.mysql.com/doc/c-api/8.0/en/mysql-real-connect.html")
+);
+HANDLEX WINAPI xll_mysql_connect(const char* host, const char* user, const char* pass, const char* db)
+{
+#pragma XLLEXPORT
+	HANDLEX h = INVALID_HANDLEX;
+
+	try {
+		handle<mysql::connect> h_(new mysql::connect(host, user, pass, db));
+		h = h_.get();
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+
+	return h;
+}
+
+AddIn xai_mysql_query(
+	Function(XLL_LPOPER4, "xll_mysql_query", "MYSQL.QUERY")
+	.Arguments({
+		Arg(XLL_HANDLEX, "db", "is a handle to a database connection."),
+		Arg(XLL_LPOPER4, "sql", "is a SQL query to execute."),
+		})
+		.Category(CATEGORY)
+	.FunctionHelp("Return the result of a query.")
+	.HelpTopic("https://dev.mysql.com/doc/c-api/8.0/en/mysql-query.html")
+);
+LPOPER4 WINAPI xll_mysql_query(HANDLEX h, const LPOPER4 psql)
+{
+#pragma XLLEXPORT
+	static OPER4 result;
+
+	try {
+		handle<mysql::connect> h_(h);
+		ensure(h_);
+		auto sql = to_string(*psql);
+		if (0 != h_->query(sql.c_str())) {
+			throw std::runtime_error(h_->error());
+		}
+		MYSQL_RES* res = mysql_store_result(*h_);
+		MYSQL_ROW ri = mysql_fetch_row(res);
+	}
+	catch (std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+
+	return &result;
+}
--- xll_mysql.h
+++ xll_mysql.h
@@ -1,8 +1,7 @@
 // xll_mysql.h - MySQL add-in
 #pragma once
 #include <exception>
-#define _ITERATOR_DEBUG_LEVEL 2
-#include <mysql.h>
+#include <mysql/mysql.h>
 #include "xll/xll/xll.h"
 
 #ifndef CATEGORY
@@ -23,32 +22,10 @@ namespace mysql {
 	};
 	inline const library_init init(0, nullptr, nullptr);
 
-	class init {
-		MYSQL* mysql_;
+	class connect {
+		MYSQL mysql;
 	public:
-		init(MYSQL* mysql = nullptr)
-			: mysql_{ mysql_init(mysql) }
-		{
-			if (!mysql_) {
-				throw std::runtime_error(mysql_error(&mysql_));
-			}
-		}
-		init(const init&) = delete;
-		init& operator=(const init&) = delete;
-		~init()
-		{
-			mysql_close(mysql_);
-		}
-		operator MYSQL* ()
-		{
-			return mysql_;
-		}
-	};
-
-	class real_connect {
-		static inline MYSQL* mysql_ = init();
-	public:
-		real_connect(/*MYSQL* mysql,*/
+		connect(
 			const char* host,
 			const char* user,
 			const char* passwd,
@@ -56,19 +33,120 @@ namespace mysql {
 			unsigned int port = 0,
 			const char* unix_socket = nullptr,
 			unsigned long client_flag = 0)
-			: mysql_()
 		{
-			mysql_ = mysql_real_connect(mysql_, host, user, passwd, db, port, unix_socket, client_flag)
-			if (!mysql_) {
-				throw std::runtime_error(mysql_error(&mysql_));
+			if (!mysql_init(&mysql)) {
+				throw std::runtime_error(mysql_error(&mysql));
+			}
+			bool t = true;
+			mysql_options(&mysql, MYSQL_ENABLE_CLEARTEXT_PLUGIN, &t);
+			if (!mysql_real_connect(&mysql, host, user, passwd, db, port, unix_socket, client_flag)) {
+				throw std::runtime_error(mysql_error(&mysql));
 			}
 		}
-		real_connect(const real_connect&) = delete;
-		real_connect& operator=(const real_connect&) = delete;
-		~real_connect()
+		connect(const connect&) = delete;
+		connect& operator=(const connect&) = delete;
+		~connect()
 		{
-
+			mysql_close(&mysql);
+		}
+		operator MYSQL* ()
+		{
+			return &mysql;
+		}
+		const char* error()
+		{
+			return mysql_error(&mysql);
+		}
+		int query(const char* sql)
+		{
+			return mysql_query(&mysql, sql);
 		}
 	};
 
 } // namespace mysql
+
+namespace xll {
+#if 0
+	inline OPER4 mysql_to_oper(const MYSQL_FIELD& f)
+	{
+		switch (f.type) {
+			//case MYSQL_TYPE_DECIMAL:
+		case MYSQL_TYPE_TINY:
+			return OPER(static_cast<unsigned char>())
+				MYSQL_TYPE_SHORT,
+				MYSQL_TYPE_LONG,
+				MYSQL_TYPE_FLOAT,
+				MYSQL_TYPE_DOUBLE,
+				MYSQL_TYPE_NULL,
+				MYSQL_TYPE_TIMESTAMP,
+				MYSQL_TYPE_LONGLONG,
+				MYSQL_TYPE_INT24,
+				MYSQL_TYPE_DATE,
+				MYSQL_TYPE_TIME,
+				MYSQL_TYPE_DATETIME,
+				MYSQL_TYPE_YEAR,
+				MYSQL_TYPE_NEWDATE, /**< Internal to MySQL. Not used in protocol */
+				MYSQL_TYPE_VARCHAR,
+				MYSQL_TYPE_BIT,
+				MYSQL_TYPE_TIMESTAMP2,
+				MYSQL_TYPE_DATETIME2,   /**< Internal to MySQL. Not used in protocol */
+				MYSQL_TYPE_TIME2,       /**< Internal to MySQL. Not used in protocol */
+				MYSQL_TYPE_TYPED_ARRAY, /**< Used for replication only */
+				MYSQL_TYPE_JSON = 245,
+				MYSQL_TYPE_NEWDECIMAL = 246,
+				MYSQL_TYPE_ENUM = 247,
+				MYSQL_TYPE_SET = 248,
+				MYSQL_TYPE_TINY_BLOB = 249,
+				MYSQL_TYPE_MEDIUM_BLOB = 250,
+				MYSQL_TYPE_LONG_BLOB = 251,
+				MYSQL_TYPE_BLOB = 252,
+				MYSQL_TYPE_VAR_STRING = 253,
+				MYSQL_TYPE_STRING = 254,
+				MYSQL_TYPE_GEOMETRY = 255
+		}
+	}
+#endif // 0
+	std::string to_string(const OPER4& o)
+	{
+		std::string s;
+
+		for (unsigned i = 0; i < o.size(); ++i) {
+			ensure(o[i].is_str());
+			s.append(o[i].val.str + 1, o[i].val.str[0]);
+			s.append(" ");
+		}
+
+		return s;
+	}
+
+	inline OPER4 query(mysql::connect& db, const char* sql)
+	{
+		OPER4 o;
+
+		if (!mysql_query(db, sql)) {
+			throw std::runtime_error(mysql_error(db));
+		}
+		MYSQL_RES* res = mysql_store_result(db);
+		if (!res) {
+			const char* err = mysql_error(db);
+			if (err) {
+				throw std::runtime_error(err);
+			}
+		}
+		else {
+			auto c = mysql_num_fields(res);
+			auto r = mysql_num_rows(res);
+			o.resize((unsigned)r, c);
+			for (unsigned i = 0; i < r; ++i) {
+				MYSQL_ROW ri = mysql_fetch_row(res);
+				MYSQL_FIELD* oi = mysql_fetch_fields(res);
+				for (unsigned j = 0; j < c; ++j) {
+					
+				}
+			}
+		}
+
+		return o;
+	}
+
+}
